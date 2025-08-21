69136e63f4a8e56c0fcb3032223e88194f889941
Ana Julia Massaro <anamassaro02@gmail.com>
2025-08-20 22:07:54 -0300

    feat: Funções de seleção/remoção/cancelamento

--- src/app/componente11/componente11.component.ts
+++ src/app/componente11/componente11.component.ts
@@ -20,7 +20,11 @@ export class Componente11Component {
   // Visibilidade dos botões
   btnCadastrar: boolean = true;
 
+  // Armazenar índice da pessoa selecionada
+  indice: number = -1;
+
   vetor:Pessoa[] = [];
+
   // Função de cadastro
   cadastrar() {
     // Cadastro no vetor
@@ -31,5 +35,54 @@ export class Componente11Component {
 
     // Visualização via console
     // console.table(this.vetor);
+  }
+
+  // Função de seleção
+  selecionar(indice: number){
+    // Atribuir o índice selecionado
+    this.indice = indice;
+
+    // Preencher o formulário com os dados da pessoa selecionada
+    this.formulario.setValue({
+      nome: this.vetor[indice].nome,
+      idade: this.vetor[indice].idade,
+      cidade: this.vetor[indice].cidade
+    });
+
+    // Visibilidade dos botões
+    this.btnCadastrar = false;
+  }
+
+  // Função de alteração
+  alterar() {
+    // Alterar o vetor com os dados do formulário
+    this.vetor[this.indice] = this.formulario.value as Pessoa;
+
+    // Limpeza dos inputs
+    this.formulario.reset();
+
+    // Voltar a visibilidade dos botões
+    this.btnCadastrar = true;
+  }
+
+  // Função de remoção
+  remover() {
+    // Remover a pessoa do vetor
+    this.vetor.splice(this.indice, 1);
+
+    // Limpeza dos inputs
+    this.formulario.reset();
+
+    // Voltar a visibilidade dos botões
+    this.btnCadastrar = true;
+  }
+
+  // Função de cancelamento
+  cancelar() {
+    // Limpeza dos inputs
+    this.formulario.reset();
+
+    // Voltar a visibilidade dos botões
+    this.btnCadastrar = true;
   }
 }
